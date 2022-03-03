2786c04b4702a20523f57833c0a22c05848fe5aa
Daian Dragos <daiandragos@yahoo.com>
2022-03-03 10:03:03 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtVCdwjNv-L-FISGYEyFORY2N9ge5tkvw",
+  authDomain: "netflix-clone-app-d170e.firebaseapp.com",
+  projectId: "netflix-clone-app-d170e",
+  storageBucket: "netflix-clone-app-d170e.appspot.com",
+  messagingSenderId: "866018915383",
+  appId: "1:866018915383:web:5e6caa6ced305c8475fbb2",
+};

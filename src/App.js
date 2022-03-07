1600008e436bda61516f00cd6882711349529626
Daian Dragos <daiandragos@yahoo.com>
2022-03-07 20:44:30 +0200

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
-import HomeScreen from "./HomeScreen";
+import React, { useEffect } from "react";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+
 import "./App.css";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        // logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // logged out
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/test" element={<h1>WhatTheFuck</h1>} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCtVCdwjNv-L-FISGYEyFORY2N9ge5tkvw",
   authDomain: "netflix-clone-app-d170e.firebaseapp.com",
@@ -6,3 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "866018915383",
   appId: "1:866018915383:web:5e6caa6ced305c8475fbb2",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = firebaseApp.firestore;
+const auth = getAuth(firebaseApp);
+
+export { auth };
+export default db;
